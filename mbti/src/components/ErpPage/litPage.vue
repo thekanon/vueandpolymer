07b01ae8695ccb2f7027d48a6bcb8757cecb02aa
do lee <dlengjs12@gmail.com>
2021-01-08 17:03:05 +0900

    lit grid 1차 작업

--- mbti/src/components/ErpPage/litPage.vue
+++ mbti/src/components/ErpPage/litPage.vue
@@ -1,8 +1,61 @@
 <template>
     <div>
-      <lit-grid :testest="arr">cdasfasdfasdf</lit-grid>
+      <table>
+          <caption>● 그리드 컴포넌트</caption>
+          <colgroup>
+              <col width="*">
+              <col width="5%">
+              <col width="5%">
+              <col width="5%">
+              <col width="5%">
+          </colgroup>
+          <tr>
+              <th></th>
+              <th><button @click="addRow" id="btnRowAdd1">행추가</button></th>
+              <th><button @click="deleteRow" id="btnRowDel1">행삭제</button></th>
+              <th><button @click="filterData" type="button" class="doaction" id="btnFilter1">필터</button></th>
+              <th><button @click="resetFilterData" type="button" class="doaction" id="btnFilter1">필터해제</button></th>
+          </tr>
+      </table>      
+      <!-- <button @click="addRow">행추가</button> -->
+      <lit-grid 
+        :bindRef ='sampleDataString' 
+        :flag='gridType'
+      ></lit-grid>
     </div>
 </template>
+<style scoped>
+  table {
+    width: 100%;
+    font-size: 15px;
+  }
+  tr {
+  }
+  button {
+    width: 100%;
+    height: 30px;
+    font-size: 13px;
+    text-transform: uppercase;
+    color: #fff;
+    background-color: #e91e63;
+    border: none;
+    border-radius: 5px;
+    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+    transition: all 0.3s ease 0s;
+    cursor: pointer;
+    outline: none;
+  }
+  button:hover {
+    background-color: #f387ac;
+    box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
+    color: #fff;
+  }
+  caption {
+    text-align: left;
+    padding-top: 20px;
+    padding-left: 10px;
+  }
+</style>
 <script>
 // import Vote from './Vote.vue';
 import '../LitElement/litGrid.js';
@@ -10,7 +63,8 @@ export default {
   name: "LitList",
 
   data: () => ({
-    arr:"ready",
+    gridType:"grid",
+    sampleDataString: '1',
     sampleData: [{"first_name":"Brendin","last_name":"Hegdonne","email":"bhegdonne0@cafepress.com","gender":"Male","ip_address":"142.101.100.170"},
 {"first_name":"Bailie","last_name":"Renhard","email":"brenhard1@vimeo.com","gender":"Male","ip_address":"18.121.110.122"},
 {"first_name":"Amandy","last_name":"Blair","email":"ablair2@newyorker.com","gender":"Female","ip_address":"230.214.198.165"},
@@ -20,9 +74,45 @@ export default {
 {"first_name":"Jeri","last_name":"Letcher","email":"jletcher6@elpais.com","gender":"Female","ip_address":"172.117.53.50"},
 {"first_name":"Pavlov","last_name":"Eydel","email":"peydel7@imgur.com","gender":"Male","ip_address":"155.47.16.22"},
 {"first_name":"Spike","last_name":"Cregan","email":"scregan8@baidu.com","gender":"Male","ip_address":"58.217.162.121"},
-{"first_name":"Liliane","last_name":"O'Corrane","email":"locorrane9@guardian.co.uk","gender":"Female","ip_address":"94.38.74.50"}]
+{"first_name":"Liliane","last_name":"O'Corrane","email":"locorrane9@guardian.co.uk","gender":"Female","ip_address":"94.38.74.50"}],
+
+
+  orgData: ''
+
   }),
+  watch: {
+    sampleData(data){
+      console.log(data);
+      this.sampleDataString = JSON.stringify(data)
+    }
+  },
+  created: function (){
+    this.sampleDataString = JSON.stringify(this.sampleData);
+  },
   methods: {
+    addRow: function() {
+      this.sampleData.push({"first_name":"Spike","last_name":"Cregan","email":"scregan8@baidu.com","gender":"Male","ip_address":"58.217.162.121"});
+    },
+    deleteRow: function() {
+      this.sampleData.pop()
+    },
+    filterData: function() {
+      console.log("Male Filter");
+      this.orgData = this.sampleData
+
+      this.sampleData = this.sampleData.filter(item => {
+        if (item.gender=='Male') {
+          return true;
+        }
+        return false;
+      });
+    },
+    resetFilterData: function() {
+      console.log("reset Filter");
+
+      this.sampleData = this.orgData;
+
+    },
     submit: function () {
       console.log(this.options)
       console.log(this.vote)
--- mbti/src/components/LitElement/litGrid.js
+++ mbti/src/components/LitElement/litGrid.js
@@ -5,51 +5,57 @@ class MyElement extends LitElement {
     // Write styles in standard CSS
     return css`
       * { color: black; }
-      .mytitle { border: 2px solid; }
+      .mytitle { border: 2px solid;background:rgb(232 232 232); }
       .mybody { border: 1px solid; }
     `;
   }
+  get bindRef() {
+    return this._bindRef
+  }
+  set bindRef(val) {
+      let oldVal = this._bindRef;
+      this._bindRef = JSON.parse(val);
+      this.requestUpdate('bindRef', oldVal);
+      this.makeGrid()
+  }
+  attributeChangedCallback(name, old, value) {
+    if (old !== value) {
+        this._attributeToProperty(name, value);
+    }
+  }
+  connectedCallback() {
+    super.connectedCallback()
+
+    //상위 컴포넌트의 데이터를 가져옴.
+    this.json =  this.parentElement.__vue__ ? this.parentElement.__vue__.sampleData : []; 
+    this.makeGrid();
+  }
   static get properties() {
     return {
-      /**
-       * The name to say "Hello" to.
-       */
       name: {type: String},
+
+      flag: {type:String},
+      bindRef:  {type:String},
+
+      json: {type:Object},
 
       /**
        * The number of times the button has been clicked.
        */
       count: {type: Number},
     };
-  }  
+  }
   constructor(){
     super();
-    this.json = [];
+    this.json = [];    
     this.body = [];
-
-    this.json = [{"first_name":"Brendin","last_name":"Hegdonne","email":"bhegdonne0@cafepress.com","gender":"Male","ip_address":"142.101.100.170"},
-    {"first_name":"Bailie","last_name":"Renhard","email":"brenhard1@vimeo.com","gender":"Male","ip_address":"18.121.110.122"},
-    {"first_name":"Amandy","last_name":"Blair","email":"ablair2@newyorker.com","gender":"Female","ip_address":"230.214.198.165"},
-    {"first_name":"Odelia","last_name":"Mompesson","email":"omompesson3@dot.gov","gender":"Female","ip_address":"147.45.68.95"},
-    {"first_name":"Kurt","last_name":"Waud","email":"kwaud4@blogspot.com","gender":"Male","ip_address":"99.250.245.64"},
-    {"first_name":"Hazel","last_name":"Courtney","email":"hcourtney5@pagesperso-orange.fr","gender":"Male","ip_address":"21.231.159.228"},
-    {"first_name":"Jeri","last_name":"Letcher","email":"jletcher6@elpais.com","gender":"Female","ip_address":"172.117.53.50"},
-    {"first_name":"Pavlov","last_name":"Eydel","email":"peydel7@imgur.com","gender":"Male","ip_address":"155.47.16.22"},
-    {"first_name":"Spike","last_name":"Cregan","email":"scregan8@baidu.com","gender":"Male","ip_address":"58.217.162.121"},
-    {"first_name":"Liliane","last_name":"O'Corrane","email":"locorrane9@guardian.co.uk","gender":"Female","ip_address":"94.38.74.50"}];
-    
-    this.body = [];
-    for(let i=0;i<this.json.length;i++){
-      this.body.push(html`
-      <tr>
-        ${Object.values(this.json[i]).map((element) => html`<td class="mybody">${element}</td>`)}
-      </tr>`);
-    }
-
-
     this.count = 10;
+    this._bindRef = '';
+
   }
   updated() {
+    console.log(this.flag)
+    console.log(this.bindRef)
   }
   render() {
     return html`
@@ -70,16 +76,26 @@ class MyElement extends LitElement {
                   <th>footer</th>
                   <th>footer</th>
                   <th>footer</th>
-                  <th>footer</th>
               </tr>
           </tfoot>
         </table>`
         : html`<table></table>`
         }
-        <button @click=${this._onClick} part="button">
+        <!-- <button @click=${this._onClick} part="button">
           ${this.count}번째 행 추가
-        </button>
+        </button> -->
         `
+  }
+  makeGrid() {
+    this.json =  this.parentElement ? this.parentElement.__vue__.sampleData : []; 
+
+    this.body = [];
+    for(let i=0;i<this.json.length;i++){
+      this.body.push(html`
+      <tr>
+        ${Object.values(this.json[i]).map((element) => html`<td class="mybody">${element}</td>`)}
+      </tr>`);
+    }
   }
   _onClick() {
     this.count++;
@@ -90,7 +106,7 @@ class MyElement extends LitElement {
     <tr>
       ${Object.values(this.json[this.json.length-1]).map((element) => html`<td class="mybody">${element}</td>`)}
     </tr>`);
-}
+  }
 
 }
 
